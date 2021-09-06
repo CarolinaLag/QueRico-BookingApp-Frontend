5446316ec95eb3eaffe80b10d742115984fadd6b
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-09-06 09:26:06 +0200

    animation fixes and checkbox

---
+++ src/components/ContactForm.test.tsx
@@ -0,0 +1,14 @@
+import { getByText, render, screen } from "@testing-library/react";
+import ContactForm from "./ContactForm";
+
+// it("on initial render, the book reservation button is disabled", () => {
+//   const { getByTestId } = render(<ContactForm />);
+
+//   expect(getByTestId("button")).toBeDisabled();
+// });
+
+// test("on initial render, the book reservation button is disabled", () => {
+//   render(<ContactForm />);
+
+//   expect(screen.getByRole("button", { name: /Boka bord/i })).toBeEnabled();
+// });
--- src/components/styles/landingPage.ts
+++ src/components/styles/landingPage.ts
@@ -12,6 +12,17 @@ export const LandingPageWrapper = styled.div`
 
 export const LandingPageImageSection = styled.section`
   background: #670101;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ImageDiv1 = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+export const ImageDiv2 = styled.div`
+  width: 100%;
 `;
 
 export const SpainImage = styled.img`
@@ -23,5 +34,17 @@ export const SpainImage = styled.img`
 export const OrangeDrinkImage = styled.img`
   width: 300px;
   height: 400px;
+  padding: 10px;
+`;
+
+export const MeatballsImage = styled.img`
+  width: 300px;
+  height: 400px;
+  padding: 10px;
+`;
+
+export const TapasImage = styled.img`
+  width: 400px;
+  height: 200px;
   padding: 10px;
 `;
